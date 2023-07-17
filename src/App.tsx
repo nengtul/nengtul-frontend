d0b63b37518d927c63e4e22fbd2c06aa930352dc
JeongYoon Yang <berry0112@naver.com>
2023-07-17 20:56:13 +0900

    로그인과 회원가입창 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
-
+import Login from "./LoginAndNewUser/Login"
+import NewUser from "./LoginAndNewUser/NewUser"
+import { BrowserRouter,Route,Routes  } from 'react-router-dom'
 function App() {
   return (
-    <>
-      <p className="ppp">안녕하세요</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/newUser' element={<NewUser/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
