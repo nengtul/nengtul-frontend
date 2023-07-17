82bbb758e21b130f8cdb7a8636c4d8ba2b68583d
jinWan0823 <dhksl823@gmail.com>
2023-07-17 19:40:47 +0900

    메인페이지 마크업

---
+++ src/mainpage/MainBanner.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Banner = styled.div`
+  width: 100%;
+  height: 230px;
+  background: #505050;
+`;
+
+export default function MainBanner() {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+}
