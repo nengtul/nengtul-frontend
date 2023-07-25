587c0486223f64c70629f5b2e46972887516fdc0
jinWan0823 <dhksl823@gmail.com>
2023-07-25 17:19:40 +0900

    🔒️ html 환경변수 사용

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
+import VitePluginHtmlEnv from "vite-plugin-html-env";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePluginHtmlEnv(), VitePluginHtmlEnv({ compiler: true })],
 });
-
-dotenv.config();
