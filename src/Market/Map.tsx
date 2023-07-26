7693ad30d383dca90bccd8e8ea27672c8994fc32
JeongYoon Yang <berry0112@naver.com>
2023-07-26 14:57:34 +0900

    회원가입창 input값 수정

--- src/Market/Map.tsx
+++ src/Market/Map.tsx
@@ -2,23 +2,6 @@ import {useEffect,useState,ReactNode}from 'react'
 const KAKAO_MAP_SCRIPT_ID='kakao-map-script'
 const KAKAO_MAP_APP_KEY='9542ff19f956b37363005f4b1adb6dbf'
 
-
-////뭘 한건진 모르겠지만 이렇게 코드 추가하니까 window.kakao.maps.load 여기에 있던 빨간줄이 해결됨
-// declare global {
-//     interface Window {
-//       kakao: KakaoNamespace; 
-//     }
-//   }
-
-// interface KakaoNamespace {
-//     maps: KakaoMaps;
-// }
-// interface KakaoMaps {
-//     load(callback: () => void): void;
-
-// }
-//---이까지
-
 interface KakaoMapScriptLoaderProps{
     children: ReactNode}
 export default function Map(props:KakaoMapScriptLoaderProps){
