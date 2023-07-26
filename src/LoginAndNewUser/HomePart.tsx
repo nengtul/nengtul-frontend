ce4cc055f456f3f42e67af5dd894e520caff7a25
jinWan0823 <dhksl823@gmail.com>
2023-07-26 20:12:06 +0900

    💄 헤더 알람 탭 메뉴 마크업

--- src/LoginAndNewUser/HomePart.tsx
+++ src/LoginAndNewUser/HomePart.tsx
@@ -1,21 +1,22 @@
-import styled from "styled-components"
-import HomeIcon from '../assets/icon/HomeIcon.svg'
-function HomePart(){
-    return(
-        <Home>
-            <HomeImg>
-                <img src={HomeIcon} alt="HomeIcon" />
-            </HomeImg>
-        </Home>
-    )
+import styled from "styled-components";
+import HomeIcon from "../assets/icon/HomeIcon.svg";
+import { Link } from "react-router-dom";
+function HomePart() {
+  return (
+    <Home>
+      <HomeImg>
+        <Link to={"/"}>
+          <img src={HomeIcon} alt="HomeIcon" />
+        </Link>
+      </HomeImg>
+    </Home>
+  );
 }
-export default HomePart
+export default HomePart;
 
-const Home= styled.div`
-
-`
-const HomeImg=styled.div`
-    padding-top: 19px;
-    padding-left:20px;
-    cursor:pointer;
-`
+const Home = styled.div``;
+const HomeImg = styled.div`
+  padding-top: 19px;
+  padding-left: 20px;
+  cursor: pointer;
+`;
