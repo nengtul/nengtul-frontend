6b7e93a5200900f48c2aba2c7685ef176e586aa8
jinWan0823 <dhksl823@gmail.com>
2023-07-26 19:15:38 +0900

    🎨 로그인 컴포넌트 분리

---
+++ src/LoginAndNewUser/LoginForm.tsx
@@ -0,0 +1,160 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // 여기서 서버로 POST 요청을 보내는 로직을 작성합니다.
+    handleLogin().catch((err) => {
+      console.error(err);
+    });
+  };
+
+  const handleLogin = async () => {
+    try {
+      const url = "http://43.200.162.72:8080/v1/login";
+      const data = {
+        email: email,
+        password: password,
+      };
+
+      const response = await axios.post(url, data);
+      console.log(response);
+    } catch (err) {
+      console.error("로그인 요청 실패", err);
+    }
+  };
+
+  return (
+    <>
+      <InputAndButton onSubmit={handleSubmit}>
+        <SquareInput id="id">
+          <IdInputWrapper>
+            <input
+              type="email"
+              placeholder="아이디를 입력해주세요"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </IdInputWrapper>
+        </SquareInput>
+        <SquareInput id="password">
+          <PasswordInputWrapper>
+            <input
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </PasswordInputWrapper>
+        </SquareInput>
+
+        <Button>
+          <SquareButton id="login" type="submit">
+            로그인
+          </SquareButton>
+        </Button>
+        <Link to="/newUser">
+          <Button>
+            <SquareButton id="newUser">회원가입</SquareButton>
+          </Button>
+        </Link>
+      </InputAndButton>
+    </>
+  );
+}
+
+const SquareInput = styled.div`
+  width: 90%;
+  height: 54rem;
+  background-color: #38db83;
+  border: 2rem solid white;
+  border-radius: 4rem;
+  margin: 0 auto;
+  margin-bottom: 3%;
+`;
+
+const IdInputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  input {
+    padding-left: 5%;
+    padding-right: 5%;
+    width: 90%;
+    height: 95%;
+    font-size: 16rem;
+    background-color: #38db83;
+    // background-color: red;
+    outline: none;
+    border: none;
+    color: white;
+  }
+
+  input::placeholder {
+    color: white;
+  }
+`;
+
+const PasswordInputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  input {
+    padding-left: 5%;
+    padding-right: 5%;
+    width: 90%;
+    height: 95%;
+
+    font-size: 16rem;
+    background-color: #38db83;
+
+    outline: none;
+    border: none;
+    color: white;
+  }
+
+  input::placeholder {
+    color: white;
+  }
+`;
+
+const SquareButton = styled.button`
+  width: 100%;
+  // height: 100%;
+  height: 51rem;
+  background-color: white;
+  border: 2rem solid white;
+  border-radius: 4rem;
+
+  font-size: 20rem;
+  font-weight: 700;
+  color: #38db83;
+  text-align: center;
+  cursor: pointer;
+  &:first-of-type {
+    margin-top: 16rem;
+  }
+`;
+const Button = styled.div`
+  width: 90%;
+  height: 6%;
+  margin: 0 auto;
+  position: relative;
+`;
+const InputAndButton = styled.form`
+  width: inherit;
+  height: inherit;
+  margin-top: 30rem;
+`;
