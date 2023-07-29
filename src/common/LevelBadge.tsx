7ea1f5db6bb895fdfeea18526393d7da19011274
jinWan0823 <dhksl823@gmail.com>
2023-07-29 22:30:23 +0900

    ⚡️ 공용 뱃지 컴포넌트 유저 포인트별 레벨 구분

--- src/common/LevelBadge.tsx
+++ src/common/LevelBadge.tsx
@@ -1,12 +1,18 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+interface LevelColors {
+  [key: string]: string;
+}
 
 interface LevelBadgeProps {
   children: string;
 }
 
-const Badge = styled.div`
+const Badge = styled.div<{ $backgroundcolor: string }>`
   padding: 4px 10px;
-  background: #c99dc2;
+  ${({ $backgroundcolor }) => css`
+    background: ${$backgroundcolor};
+  `};
   font-size: 15rem;
   color: #fff;
   font-weight: 700;
@@ -16,9 +22,28 @@ const Badge = styled.div`
 `;
 
 export default function LevelBadge({ children }: LevelBadgeProps) {
+  const point = Number(children);
+  let level;
+  if (point <= 100) {
+    level = "막내요리사";
+  } else if (point <= 200) {
+    level = "보조요리사";
+  } else if (point <= 300) {
+    level = "조리장";
+  } else {
+    level = "주방장";
+  }
+
+  const levelColors: LevelColors = {
+    막내요리사: "#c99dc2",
+    보조요리사: "#FC8F6C",
+    조리장: "#8FB2F7",
+    주방장: "#38DB83",
+  };
+
   return (
     <>
-      <Badge>{children}</Badge>
+      <Badge $backgroundcolor={levelColors[level]}>{level}</Badge>
     </>
   );
 }
