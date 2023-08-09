a347acfd9ecb65e19bf64da80643a7fa7d2f6ff1
jinWan0823 <dhksl823@gmail.com>
2023-08-09 15:33:03 +0900

    🎨 useSelector 토큰 호출

--- src/url.ts
+++ src/url.ts
@@ -1,22 +1,21 @@
 //회원가입/로그인부분
-export const USER_JOIN_URL="/api.shop/v1/users/join";
-export const USER_DETAIL_URL="/api/v1/users/detail";
-export const USER_VERIFY_RESET_URL="/api/v1/users/verify/reset";
-export const USER_LOGIN_URL="/api/v1/users/login";
-export const USER_DETAIL_PASSWORD_URL="/api/v1/users/detail/password";
-export const USER_FINDID_URL="/api/v1/users/findid";
-export const USER_FINDPW_URL="/api/v1/users/findpw";
+export const USER_JOIN_URL = "/api.shop/v1/users/join";
+export const USER_DETAIL_URL = "/api/v1/users/detail";
+export const USER_VERIFY_RESET_URL = "/api/v1/users/verify/reset";
+export const USER_LOGIN_URL = "/api/v1/users/login";
+export const USER_DETAIL_PASSWORD_URL = "/api/v1/users/detail/password";
+export const USER_FINDID_URL = "/api/v1/users/findid";
+export const USER_FINDPW_URL = "/api/v1/users/findpw";
 
-export const AUTH_REFRESH_URL="/api/v1/auth/refresh"
+export const AUTH_REFRESH_URL = "/api/v1/auth/refresh";
 
 //나눔게시판 부분
-export const SHAREBOARD_URL="/api/v1/shareboards";
-export const SHARE_MYLIST_URL="/api/v1/shareboards/mylist";
+export const SHAREBOARD_URL = "/api/v1/shareboards";
+export const SHARE_MYLIST_URL = "/api/v1/shareboards/mylist";
 
 //공지사항 부분
-export const NOTICES_LIST_URL="/api/v1/notices/list";
-export const NOTICES_URL="/api/v1/notices";
-
+export const NOTICES_LIST_URL = "/api/v1/notices/list";
+export const NOTICES_URL = "/api/v1/notices";
 
 //레시피 부분
-export const RECIPE_URL="/api/v1/recipe"
+export const RECIPE_URL = "/api/v1/recipe";
