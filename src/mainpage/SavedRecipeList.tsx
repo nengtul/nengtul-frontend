45f6f999d652d24e4ded4885ca4784e4897f4db0
jinWan0823 <dhksl823@gmail.com>
2023-08-12 22:30:08 +0900

    💄 삭제 버튼 css 수정

--- src/mainpage/SavedRecipeList.tsx
+++ src/mainpage/SavedRecipeList.tsx
@@ -1,39 +1,39 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import  {deleteData} from "../axios";
+import { deleteData } from "../axios";
 import { Post } from "./MyRecipe";
 import { useSelector } from "react-redux";
 import { RootState } from "../Store/store";
 import { SAVED_RECIPE_URL } from "../url";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 interface RecipeListCardProps {
   post: Post;
   onDeletePost: (postId: number) => void;
 }
 
-export default function SavedRecipeList({ post,onDeletePost }: RecipeListCardProps) {
-  const Token=useSelector((state: RootState)=>state.accessTokenValue)
-  const {accessTokenValue}=Token;
-  const MY_TOKEN=accessTokenValue;
-  console.log('post',post)
-  const onDelete=()=>{
-    if(MY_TOKEN!==null){
-      deleteData(`${SAVED_RECIPE_URL}/${post.id}`,MY_TOKEN)
-      .then(()=>{
-          console.log('삭제되었습니다')
-          onDeletePost(post.id)
-      })
-      .catch(error=>{
-          console.log(error)
-      })
+export default function SavedRecipeList({ post, onDeletePost }: RecipeListCardProps) {
+  const Token = useSelector((state: RootState) => state.accessTokenValue);
+  const { accessTokenValue } = Token;
+  const MY_TOKEN = accessTokenValue;
+  console.log("post", post);
+  const onDelete = () => {
+    if (MY_TOKEN !== null) {
+      deleteData(`${SAVED_RECIPE_URL}/${post.id}`, MY_TOKEN)
+        .then(() => {
+          console.log("삭제되었습니다");
+          onDeletePost(post.id);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-  }
+  };
   return (
     <>
       <RecipeLi>
-        <button 
-          className="delete-image-btn"
-          onClick={onDelete}>
-            X
+        <button className="delete-image-btn" onClick={onDelete}>
+          <FontAwesomeIcon icon={faTrash} />
         </button>
         <RecipeLink to={`/${post.recipeId}`}>
           <img src={post.thumbnailUrl} alt="food-img" />
@@ -47,50 +47,56 @@ export default function SavedRecipeList({ post,onDeletePost }: RecipeListCardPro
   );
 }
 
-
 const RecipeLi = styled.li`
   width: 100%;
   border-bottom: 1px solid #dad7d7;
-  height:104px;
-  position:relative;
-  .delete-image-btn{
-    width:20rem;
-    height:20rem;
-    background-color:rgb(254, 98, 98);
-    color:white;
-    border-radius:100%;
-    font-size:10rem;
-    position:absolute;
-    top:3px;
-    left:2rem;
-    cursor:pointer
-}
+  height: 104px;
+  position: relative;
+  .delete-image-btn {
+    padding: 6px 20px;
+    background-color: rgb(255 158 158);
+    color: white;
+    position: absolute;
+    bottom: 6px;
+    right: 4px;
+    cursor: pointer;
+    font-size: 12rem;
+    border-radius: 5px;
+  }
 `;
 
 const RecipeInfo = styled.div`
-  padding: 20px 14px;
+  padding: 20px 8px;
   width: 100%;
 `;
 
 const RecipeLink = styled(Link)`
   width: 100%;
   display: flex;
-  img{
-    width:104px;
-    height:104px;
+  img {
+    width: 104px;
+    height: 104px;
   }
 `;
 
 const RecipeTit = styled.p`
-  font-size: 15rem;
+  font-size: 16rem;
   color: #5b5b5b;
   font-weight: 800;
+  line-height: 1.3;
+  max-height: 2.6;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  display: -webkit-box;
+  word-break: keep-all;
 `;
 
-
-
 const RecipeWriter = styled.span`
-  font-size: 14rem;
-  font-weight: 800;
+  display: block;
+  font-size: 13rem;
+  font-weight: 700;
+  margin-top: 10px;
   color: #5b5b5b;
 `;
